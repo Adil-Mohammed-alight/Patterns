93c8aae5660e2842376d5a6c2776a7cb2dee161a
adil <adiljamal88@gmail.com>
2022-10-25 18:34:53 +0530

    Complete Assignment

--- Assignment.java
+++ Assignment.java
@@ -1,10 +1,10 @@
 public class Assignment {
-    public static void main(String[] args){
-        int n = 10;
 
+
+    public static void Pattern(int n){
         for (int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if ((i==0 & j>0&& j<n-1) || ((j==0 || j==n-1) && i>0) || (i==(n-1)/2 && j>0 && j<n-1)) {
+                if (i == 0 & j > 0 && j < n - 1 || (j == 0 || j == n - 1) && i > 0 || i == (n - 1) / 2) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -17,7 +17,7 @@ public class Assignment {
             System.out.print("  ");
 
             for(int j=0; j<n; j++){
-                if ((j==0) || ((i==0 || i==n-1 ) && j>0 && j<n-1) || (j==n-1 && i>0 && i<n-1)) {
+                if (j == 0 || (i == 0 || i == n - 1) && j < n - 1 || j == n - 1 && i > 0 && i < n - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -84,7 +84,7 @@ public class Assignment {
             System.out.print("  ");
 
             for(int j=0; j<n; j++){
-                if ((j==0 || (i==0 && j>0)) || (i==(n-1)/2 && j>0) || (i==n-1 && j>0)) {
+                if (j == 0 || i == 0 || i == (n - 1) / 2 || i == n - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -153,6 +153,86 @@ public class Assignment {
 
             System.out.println();
         }
+    }
+
+    public static void PatternOne(int n1) {
+
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <=4; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void PatternTwo(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || j == 0 || j == n - 1 || i == n - 1) || (i + j <= (n - 1) / 2) || (j - i > (n - 1) / 2)) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void PatternForth(int n ){
+
+        for (int i = 0; i < n; i++) {
+            for(int j=0;j<n;j++){
+
+                if ((i-j>=(n-1)/2 || i==n-1)  || (i+j<=(n-1)/2) || i==0){
+
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+
+
+        }
+    }
+
+    public static void PatternThree (int n) {
+        for (int i = 0; i < n; i++) {
+            for(int j=0;j<n;j++){
+
+                if ((i-j>=(n-1)/2) ||(i+j>=(n+(n-1)/2 -1) )){
+
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        int n = 14;
+//        Pattern(n);
+
+//        PatternOne(n);
+//        PatternTwo(n);
+//        PatternThree(n);
+//        PatternForth(n);
+
 
 
 
